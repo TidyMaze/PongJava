0425a34f6d3c8154c198d7e6a6b6cab57a6cc525
TidyMaze <rolland_yann@orange.fr>
2015-05-09 16:38:20 +0200

    Alignement des murs

--- pong/src/Utils/CalcUtils.java
+++ pong/src/Utils/CalcUtils.java
@@ -1,8 +1,31 @@
 package Utils;
 
+import java.util.HashMap;
+
+import model.Position;
+
 public class CalcUtils {
+
 
   public static double moy(double a, double b) {
     return (a + b) / 2;
+  }
+
+  public static double alignX(Position position, int wallWidth) {
+    HashMap<Position, Double> map = new HashMap<>();
+    map.put(Position.LEFT, 0.0);
+    map.put(Position.TOP, 0.0);
+    map.put(Position.RIGHT, 100.0 - wallWidth);
+    map.put(Position.BOTTOM, 0.0);
+    return map.get(position);
+  }
+
+  public static double alignY(Position position, int wallWidth) {
+    HashMap<Position, Double> map = new HashMap<>();
+    map.put(Position.LEFT, 0.0);
+    map.put(Position.TOP, 0.0);
+    map.put(Position.RIGHT, 0.0);
+    map.put(Position.BOTTOM, 100.0 - wallWidth);
+    return map.get(position);
   }
 }
--- pong/src/model/WallFactory.java
+++ pong/src/model/WallFactory.java
@@ -1,18 +1,30 @@
 package model;
 
+import Utils.CalcUtils;
+
 public class WallFactory {
-
-
 
   private static final int WALL_WIDTH = 5;
 
   public Wall createWall(Position position) {
-    switch (position) {
-      case LEFT:
-        return new Wall(0, 0, 100, WallFactory.WALL_WIDTH);
-      default:
-        break;
-    }
-    return null;
+    double x = CalcUtils.alignX(position, WALL_WIDTH);
+    double y = CalcUtils.alignY(position, WALL_WIDTH);
+    return createWallAligned(position, x, y);
+  }
+
+  private Wall createWallAligned(Position position, double x, double y) throws RuntimeException {
+    if (isVertical(position))
+      return new Wall(x, y, WALL_WIDTH, 100);
+    if (isHorizontal(position))
+      return new Wall(x, y, 100, WALL_WIDTH);
+    throw new RuntimeException("Position invalide : " + position);
+  }
+
+  private boolean isHorizontal(Position position) {
+    return position == Position.TOP || position == Position.BOTTOM;
+  }
+
+  private boolean isVertical(Position position) {
+    return position == Position.LEFT || position == Position.RIGHT;
   }
 }
